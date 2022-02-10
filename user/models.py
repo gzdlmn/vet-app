6ad195b3ec4bd252419b255a9550d78cc1dcc0e0
gzdlmn <gzdlmn01@gmail.com>
2022-02-11 00:15:44 +0300

    created a petowner model for each user

--- user/models.py
+++ user/models.py
@@ -1,14 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here. user modeline ek alanlar eklemek için onetonone ile ilşkilendiriyorum. tel address hazır user modelinde yok
-
+#I will add some fields to the ready user model. So onetoone or foreignkey.
+#petowner means that who has a pet.
 class Petowner(models.Model):
-    user = models.OneToOneField(User, null=True, blank=False, verbose_name="Kullanıcı", on_delete=models.CASCADE)
-    tel = models.CharField(max_length=13, verbose_name="Tel")
-    address = models.CharField(max_length=100, verbose_name="Adres")
+    user = models.OneToOneField(User, null=True, blank=False, verbose_name="User", on_delete=models.CASCADE)
+    tel = models.CharField(max_length=13, verbose_name="Phone")
+    address = models.CharField(max_length=100, verbose_name="Address")
     class Meta:
-        verbose_name_plural = "Kullanıcılar Ek Bilgiler"
+        verbose_name_plural = "Additional information for the user"
 
     def __str__(self):
         return "{} - {} - {}".format(self.user, self.tel, self.address)
