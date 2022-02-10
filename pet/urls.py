04e6dbc15f6b42e918d944bab002e7614a972e7e
gzdlmn <gzdlmn01@gmail.com>
2022-02-11 00:09:33 +0300

    created urls for pet

--- pet/urls.py
+++ pet/urls.py
@@ -1,12 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
-#url leri dinamik yazmak için app_name vermek önemli
+#app_name is very important in django. Because we use dynamic url. {% url 'pet:pets' %} instead of /pet/pets. We don't write like that!
 app_name = "pet"
 
 urlpatterns = [
     path('pets/', views.all_pets, name='pets'),
-    #path('dashboard/', views.dashboard, name='dashboard'),
     path('addpet/', views.add_pet, name='addpet'),
     path('detail/<int:id>', views.detail, name='detail'),
     path('update/<int:id>', views.update, name="update"),
