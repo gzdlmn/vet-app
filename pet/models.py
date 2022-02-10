63d7cecc9d8fa24d3aa64938bd1642d7deb83053
gzdlmn <gzdlmn01@gmail.com>
2022-02-10 23:51:20 +0300

    created pet model, added filefield

--- pet/models.py
+++ pet/models.py
@@ -3,19 +3,17 @@ from django.contrib.auth.models import User
 from user.models import Petowner
 
 
-
-
 # Create your models here.
-#Bir hayvan modeli oluşturduk. Django bize user modelini hazır verdiği için hazır modeli bu modele bağladık.
-#Her hayvanın bir sahibi olacak. Buna da petowner dedik.on_delete ise kullanıcı silinirse ona ait hayvanı da sil demek.
-#created_date ile kliniğe katılma tarihini otomatik aldık.
+#created pet model. I used user model which is in django.
+#there is a petowner for each pet. if petowner is deleted, pet will delete. (on_delete means that)
+#created_date automatically create.
 class Pet(models.Model):
     petowner = models.ForeignKey("auth.user", on_delete=models.CASCADE)
-    image = models.FileField(null=True, blank=True, verbose_name="Resim")
+    image = models.FileField(null=True, blank=True, verbose_name="Image")
     #Bilerek null blank = True bıraktık. Resim eklemek zorunde değil. Default bir resim koyacağız eklemezse.
-    type = models.CharField(max_length=20, verbose_name="Tür")
-    genius = models.CharField(max_length=20, verbose_name="Cins")
-    name = models.CharField(max_length=20, verbose_name="İsim")
-    age = models.CharField(max_length=4, verbose_name="Yaş")
-    explanation = models.TextField(verbose_name="Açıklama")
-    created_date = models.DateField(auto_now_add=True, verbose_name="Katılma Tarihi")
+    type = models.CharField(max_length=20, verbose_name="Type")
+    genius = models.CharField(max_length=20, verbose_name="Genius")
+    name = models.CharField(max_length=20, verbose_name="Name")
+    age = models.CharField(max_length=4, verbose_name="Age")
+    explanation = models.TextField(verbose_name="Explanation")
+    created_date = models.DateField(auto_now_add=True, verbose_name="Created Date")
